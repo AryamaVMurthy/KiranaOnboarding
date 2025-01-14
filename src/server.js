7260500f157d2d0c80bf8ad4f269673206549684
Pradyun D <74104467+Prady007-1981@users.noreply.github.com>
2025-01-14 22:01:45 +0530

    Updated server.js
    Does not include the middleware

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import productRoutes from './routes/productRoute.js'
+import orderRoutes from './routes/orderRoutes.js'
+import orderRegRoutes from './routes/orderRegRoutes.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(express.json());
+
+app.use('/product',productRoutes)
+app.use('/ordersReg',orderRegRoutes)
+app.use('/orders',orderRoutes)
+
+
+app.listen(PORT, () => {
+    console.log(`Server has started on port: ${PORT}`)
+})
