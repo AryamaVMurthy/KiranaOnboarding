4e9fb5477ca1711cfe2316cc8d6bac91aab94003
Pradyun D <74104467+Prady007-1981@users.noreply.github.com>
2025-01-15 23:02:14 +0530

    Delete src

--- src/prisma/migrations/20250112130939_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "order" ALTER COLUMN "lastUpdate" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "product" ALTER COLUMN "lastUpdate" DROP NOT NULL;
--- src/prisma/migrations/20250112183155_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `appoval` on the `order` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "order" DROP COLUMN "appoval",
-ADD COLUMN     "approval" BOOLEAN NOT NULL DEFAULT false;
